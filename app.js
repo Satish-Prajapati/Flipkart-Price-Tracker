161a85f83404b9c13f275a66e3928e9030d0de91
Satish196 <satish196@hotmail.com>
2020-04-14 23:41:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const Products = require("./models/products");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 
@@ -35,7 +37,39 @@ app.get("/tracker", async (req, res) => {
 		const products = await Products.find({});
 		res.status(200).render("tracker", { products });
 	} catch (error) {
-		res.status(500).send();
+		res.status(500).send(error);
+	}
+});
+
+app.patch("/update/:id", async (req, res) => {
+	// console.log(req.body);
+	try {
+		const product = await Products.findOneAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!product) {
+			return res.status(404).send();
+		}
+
+		res.send(product);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.delete("/delete/:id", async (req, res) => {
+	try {
+		const product = await Products.findByIdAndDelete(req.params.id);
+
+		if (!product) {
+			return res.status(404).send();
+		}
+
+		res.status(200).send(product);
+	} catch (error) {
+		res.status(500).send(error);
 	}
 });
 
